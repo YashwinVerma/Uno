02e877c4e5bc2a66e2fe2a38eb5dd9cc902adb4e
YashwinVerma <23Verma_Y@bexleygs.co.uk>
2024-10-06 12:22:34 +0100

    Update main.py
    Made a lot of changes to the appearence of each card and generl class structure.

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ def main():
             print("Wrong datatype. Try again!")
         if ((total_players < 2) or (total_players > 8)):
             print("Invalid number of players selected!")
+        else:
+            break
         clear_screen()
     for i in range(total_players):
         while True:
             reject_flag_1 = False
             new_player = input("Enter a good name for the player: ").replace(" ", "")
             exit_statement(new_player)
-            if (len(new_player) < 20) or (len(new_player) > 1):
+            if ((len(new_player) < 20) or (len(new_player) > 1)):
                 if (new_player in player_order_list):
                     reject_flag_1 = True
                 else:
@@ -39,36 +41,35 @@ def main():
     random.shuffle(running_card_deck)
     full_card_deck = running_card_deck
     while True:
+        clear_screen()
         reject_flag_2 = False
         try:
             card_number = int(input("Enter the number of cards you would like to start with for each player in integer form eg(5). Maximum 11 and minimum 4: "))
-            if (card_number > 12) or (card_number < 4):
+            if ((card_number > 12) or (card_number < 4)):
                 reject_flag_2 = True
         except ValueError:
             reject_flag_2 = True
-        if reject_flag_2 is True:
+        if (reject_flag_2 is True):
             print("Your input is either of the wrong datatype or is too large or small.")
         else:
             for i in player_order_list:
                 for _ in range(card_number):
-                    new_card_add = random.choice(running_card_deck)
-                    new_card_add.card_possession = i.player_name
-                    running_card_deck.remove(new_card_add)
-    
+                    i.add_card(random.choice(running_card_deck), running_deck)
+
 def input_func(choices):
     selected_index = 0  # Initialize selected_index
     while True:
         for index, choice in enumerate(choices):
-            if index == selected_index:
+            if (index == selected_index):
                 print(f"> {choice} <")
             else:
                 print(f"  {choice}")
         user_input = input("Press 'w' for Up, 's' for Down, or 'Enter' to select: ")
-        if user_input == 'w':
+        if (user_input == 'w'):
             selected_index = (selected_index - 1) % len(choices)
-        elif user_input == 's':
+        elif (user_input == 's'):
             selected_index = (selected_index + 1) % len(choices)
-        elif user_input == '':
+        elif (user_input == ''):
             return choices[selected_index]  # Return the selected choice
         else:
             print("Invalid response")
@@ -81,11 +82,11 @@ def create_deck():
     special_cards = ["SKIP_TURN", "REVERSE_TURN", "DRAW_TWO"]
     wild_cards = ["WILD", "DRAW_FOUR"]
     card_deck = []
-    for color in card_color:
+    for color in card_colors:
         for number in card_numbers:
-            card_deck.append(Card("GENERIC_CARD", color, number, None))
+            card_deck.append(Card("GENERIC_CARD", color, number, None, f"{Color.color}{color.lower().capitalize()} {number} {Color.RESET}"))
         for special_card in special_cards:
-            card_deck.extend([Card(special_card, color, None, None)] * 2)
+            card_deck.extend([Card(special_card, color, None, None, f"{Color.RED}W{Color.YELLOW}I{Color.GREEN}L{Color.BLUE}D{Color.RESET}") if special_card == "WILD" else "Draw four"] * 2)
     for _ in range(4):
         for i in wild_cards:
             card_deck.append(Card(i, None, None, None))
@@ -95,18 +96,19 @@ def turn_manager(action_type):
     pass
 
 class Card:
-    def __init__(self, card_type, card_color, card_number, card_possession):
+    def __init__(self, card_type, card_color, card_number, card_possession, card_appearence):
         self.card_type = card_type
         self.card_color = card_color
         self.card_number = card_number
         self.card_possession = card_possession
+        self.card_appearence = card_appearence
 
     def match_cards(self, matching_object, current_player, current_running_color, request_color):
         color_sensitive_cards = ["SKIP_TURN", "REVERSE_TURN", "DRAW_TWO"]
         if (self.card_type != "GENERIC_CARD"):
             if (self.card_type in color_sensitive_cards):
-                if self.card_color == current_running_color:
-                    if self.card_type == "DRAW_TWO":
+                if (self.card_color == current_running_color):
+                    if (self.card_type == "DRAW_TWO"):
                         current_player.draw_total += 2
                         return True
                     else:
@@ -129,10 +131,25 @@ class Player:
         self.player_cards = []
         self.player_card_number = len(self.player_cards)
         self.draw_total = 0
+    def add_card(self, new_card_add, running_deck):
+        self.player_cards.append(new_card_add)
+        new_card_add.card_possession = self.player_name
+        running_deck.remove(new_card_add)
+    def select_card(self, compare_card):
+        pass
+
+class Color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
 
 def clear_screen():
     os.system('cls')
 
 def exit_statement(statement_check):
-    if statement_chanck.upper().replace(" ", "") == "EXIT":
+    if (statement_check.upper().replace(" ", "") == "EXIT"):
         exit()
